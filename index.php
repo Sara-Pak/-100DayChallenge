990788cacea9969fd6760dc3f10864519949f0da
Sara Pak <94329581+Sara-Pak@users.noreply.github.com>
2022-06-30 00:03:07 -0400

    Update index.php
    added for while loop, arrays and indexed arrays

--- index.php
+++ index.php
@@ -18,8 +18,9 @@ echo '<br>';
 echo $num;
 
 echo '<br>';
-// constants -> an identifier (name) for a value
-// creating constants
+/* constants -> an identifier (name) for a value
+*  creating constants
+*/
     define('constant', 'This is a constant');    
     define('number', 99, true);
 
@@ -54,38 +55,60 @@ $var = "string";
 $var = null;
 var_dump($var);
 
-//if elseif else
-//switch
+/* if elseif else:
+*  if (condition)
+*   {
+*  action
+*   }
+*/
 
-//if (condition){
-    //action
-//}
-
-//----------------------------------------------------------------
 $age = 18;
-if ($age===12)
-    {
-        echo "you should not be here";
-    } elseif ($age===18) 
-    {
-        echo "you're 18 years old, your perfectly welcome";
-    }  
-     else {
-        echo "you are welcome here";
+
+if ($age===12){
+    echo "you should not be here";
+    } elseif ($age===18) {
+    echo "you're 18 years old, your perfectly welcome";
+    } else {
+    echo "you are welcome here";
     }
 
-    //----------------------------------------------------------------
-    //while and the for loops
-    //initialize first
-    //while (//condition) {}
-        //the increment or the decrement
-
+    /* while and the for loops
+    *  initialize first
+    *  while (//condition) {}
+    *  the increment or the decrement
+    */
         $i = 0;
         while ($i <= 10) {
-            // increment 
-            echo $i++;
+            echo $i++; //increment
             echo "<br>";
         }
 
+        $i = 20;
+        while ($i < 0) {
+            echo $i--; //decrement
+            echo "<br>";
+        }
+        
+        $i = 20;
+        while ($i > 0) {
+            echo "number is $i <br>";
+            $i--;
+        }
+        for ($i = 0; $i<=20; $i++) {
+            echo " value is: $i <br>";
+        }
+
+    /*  arrays and indexed arrays 
+        indexed | associative array | multide
+    */
+        //indexed
+        $users = ["jane", "mohamed", "raj", "jocey"];
+        echo $users[2]; //raj
+        echo count($users);
+
+        //associative array
+        $age = ['ahmed' => 33, 'jane' => 22, 'raj' => 23];
+
+        
 
 ?> 
